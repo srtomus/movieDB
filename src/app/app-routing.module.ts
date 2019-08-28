0aae9f8e7c76c7fe763e96ef72aadca78b178773
Vlad <vladitor31@gmail.com>
2019-08-28 11:57:33 +0200

    Home eliminado y rutas creadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'movies-list', pathMatch: 'full' },
   { path: 'movies-list', loadChildren: './movies-list/movies-list.module#MoviesListPageModule' },
   { path: 'movie', loadChildren: './movie/movie.module#MoviePageModule' },
 ];
